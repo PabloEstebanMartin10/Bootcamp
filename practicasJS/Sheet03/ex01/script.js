74c1adf34776d2d144b45b455f5121a5eb4be611
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-15 18:44:14 +0200

    ejercicio completado

--- practicasJS/Sheet03/ex01/script.js
+++ practicasJS/Sheet03/ex01/script.js
@@ -1,1 +1,6 @@
+const a = [3,1,2];
+const b = a.slice(0,1);// b = [3], a = [3,2,1] 
+const c = b.toReversed();//c = [3], b = [3] 
+const d = a.with(0,99);//d = [99,2,1], a=[3,2,1]
+//ninguno modifica la variable origiinal, por lo tanto todos son inmutables
 
