05ae5a5e47f70d26b9e068642d9914dbc4d08573
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-15 15:33:44 +0200

    ejercicio completado

--- practicasJS/Sheet02/ex06/script.js
+++ practicasJS/Sheet02/ex06/script.js
@@ -1,4 +1,8 @@
-
-function saludar(ciudad){
-
+function saludar(ciudad) {
+	console.log(`Soy ${this.nombre} de ${ciudad}`);
 }
+const persona = { nombre: "Lucía" };
+saludar.call(persona, "Madrid");
+saludar.apply(persona, ["Sevilla"]);
+const persona2 = {nombre: "Ada"}
+
