b43e220b6db7d54638afedb215e82330e6909706
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-15 15:54:57 +0200

    implementado import en main.js

---
+++ practicasJS/Sheet02/ex07/main.js
@@ -0,0 +1,7 @@
+import sumar from "./math";
+import { areaCirculo } from "./math";
+import { media } from "./math";
+
+console.log('SUMA', sumar(2,3)); // 5
+console.log('MEDIA', media(2,'x',4)); // 3
+console.log('AREA', areaCirculo(2)); // ~12.56
