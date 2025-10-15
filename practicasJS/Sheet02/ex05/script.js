0da48202b57dc016e2501233ebd84c854b3e1a1f
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-15 15:15:24 +0200

    ejercicio completado

---
+++ practicasJS/Sheet02/ex05/script.js
@@ -0,0 +1,11 @@
+const temporizador = {
+	nombre: "",
+	iniciar(ms) {
+        let s = ms*1000
+		setTimeout(() => {
+			console.log(`Listo:${this.nombre}`);
+		}, s);
+	},
+};
+
+const t = Object.create(temporizador); t.nombre='Tarea X'; t.iniciar(2);
