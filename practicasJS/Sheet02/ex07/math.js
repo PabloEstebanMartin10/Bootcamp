912ae88d0b6826b89fd12be3f30e0439f2b96abb
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-15 15:47:42 +0200

    feat: implementados sumar, areaCirculo y media en math.js

---
+++ practicasJS/Sheet02/ex07/math.js
@@ -0,0 +1,19 @@
+export default function sumar(a, b) {
+	return a + b;
+}
+export function areaCirculo(r) {
+	return r * PI;
+}
+export function media(...nums) {
+	let nElements = 0;
+	let sum = 0;
+	nums.forEach((num) => {
+		if (num === Number) {
+			nElements++;
+            sum = sum+num;
+		}
+	});
+    return sum/nElements;
+}
+
+const PI = 3.1416;
