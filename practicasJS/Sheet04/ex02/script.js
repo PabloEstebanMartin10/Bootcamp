334078a392addc6fe5a5146a5a4cde34207e6410
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-16 19:05:17 +0200

    feat: checkvalidity al input cuando se pulsa el botón

--- practicasJS/Sheet04/ex02/script.js
+++ practicasJS/Sheet04/ex02/script.js
@@ -14,3 +14,15 @@ li2.textContent = "Practica DOM";
 //add elementos al html
 listaTareas.append(li1, li2);
 contenedor.append(subtitulo, listaTareas);
+
+//ejercicio 2
+const formInput = document.getElementsByTagName("input")[0];
+formInput.setAttribute("required","");
+formInput.setAttribute("pattern", `(.|\\s)*\\S(.|\\s)*`)
+
+document.querySelector("#form-tarea").addEventListener("click", (e) => {
+    e.preventDefault();
+	if (e.target.type == "submit") {
+       console.log(formInput.checkValidity()) ;
+	}
+});
