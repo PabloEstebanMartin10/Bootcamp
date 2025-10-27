e8fef2b769215bd920ab5ffff4d23cd3b6559e04
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-27 18:21:50 +0100

    ejercicio completado

--- practicasReact/Sheet02/src/App.tsx
+++ practicasReact/Sheet02/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import MensajePersonalizado from "./exercises/ex01/MensajePersonalizado";
+import Tarjeta from "./exercises/ex02/Tarjeta";
+import Cabecera from "./exercises/ex02/Cabecera";
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
 			{/* <MensajePersonalizado color="blue" texto="Texto aqui" /> */}
+			<Tarjeta>
+				<Cabecera>Mi título</Cabecera>
+				<p>Este es el contenido</p>
+			</Tarjeta>
 		</>
 	);
 }
---
+++ practicasReact/Sheet02/src/exercises/ex02/Cabecera.tsx
@@ -0,0 +1,7 @@
+function Cabecera({children}:{children:React.ReactNode}) {
+    return<h1 style={{backgroundColor: "red"}}>
+        {children}
+    </h1>
+}
+
+export default Cabecera
