aaf2fe956574f6d1165007eccfa90d2579d197fe
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-10-28 15:56:31 +0100

    ejercicio básico

--- practicasReact/Sheet03/src/App.tsx
+++ practicasReact/Sheet03/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Clock from "./exercises/ex01/Clock";
 import Timer from "./exercises/ex01/Timer";
+import Greeting from "./exercises/ex02/Greeting";
 
 function App() {
+	const [nombre, setNombre] = useState("");
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setNombre(e.target.value);
+	}
 	return (
 		<>
+			{/* <Clock/>
+			<Timer/> */}
 
-			<Clock/>
-			<Timer/>
+			<input type="text" value={nombre} onChange={handleChange} />
+			<Greeting nombre={nombre} />
 		</>
 	);
 }
