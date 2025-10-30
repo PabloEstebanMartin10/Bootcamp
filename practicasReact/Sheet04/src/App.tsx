9a48d8e019b7fde45132128872588bfe82ac9026
Pablo Esteban <pabloestebanm.sanjosemlg@fundacionloyola.net>
2025-10-30 14:17:05 +0100

    feature: lista mostrada

--- practicasReact/Sheet04/src/App.tsx
+++ practicasReact/Sheet04/src/App.tsx
@@ -2,81 +2,102 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./exercises/Navbar";
 import type { Pokemon } from "./exercises/pokemon";
+import Cards from "./exercises/Cards";
 
 interface PokeBasic {
-	name: string;
-	url: string;
+  name: string;
+  url: string;
 }
 
 function App() {
-	const [pokenames, setPokenames] = useState<PokeBasic[]>([]);
-	const [pokeList, setPokeList] = useState<Pokemon[]>([]);
-	const [searchStr, setSearchStr] = useState("");
-	const pokeApiURL = `https://pokeapi.co/api/v2/pokemon?offset=${0}&&limit=${21}`;
-	function filterStrChanged(e: React.ChangeEvent<HTMLInputElement>) {
-		setSearchStr(e.target.value);
-	}
-	async function fetchData(URL: string) {
-		const response = await fetch(URL);
-		if (!response.ok) throw new Error("Connection error");
-		const data = await response.json();
-		return data;
-	}
-	useEffect(() => {
-		(async () => {
-			const response = await fetchData(pokeApiURL);
-			setPokenames(response.results);
-		})();
-	}, []);
+  const [pokenames, setPokenames] = useState<PokeBasic[]>([]);
+  const [pokeList, setPokeList] = useState<Pokemon[]>([]);
+  const [searchStr, setSearchStr] = useState("");
+  const pokeApiURL = `https://pokeapi.co/api/v2/pokemon?offset=${0}&&limit=${21}`;
+  function filterStrChanged(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchStr(e.target.value);
+  }
+  async function fetchData(URL: string) {
+    const response = await fetch(URL);
+    if (!response.ok) throw new Error("Connection error");
+    const data = await response.json();
+    return data;
+  }
+  useEffect(() => {
+    (async () => {
+      const response = await fetchData(pokeApiURL);
+      setPokenames(response.results);
+    })();
+  }, []);
 
-	async function getPokeData() {
-		const urls = pokenames.map((x) => fetch(x.url));
-		const promises = await Promise.allSettled(urls);
-		const jsonPromises = promises
-			.filter((x) => x.status === "fulfilled")
-			.map((x) => x.value.json());
-		const pokeInfo = await Promise.all(jsonPromises);
+  async function getPokeData() {
+    const pokePromises = pokenames.map((x) => fetch(x.url));
+    const promises = await Promise.allSettled(pokePromises);
+    const jsonPromises = promises
+      .filter((x) => x.status === "fulfilled")
+      .map((x) => x.value.json());
+    const pokeInfo = await Promise.all(jsonPromises);
 
-		const speciesURL = pokeInfo.map((x) => x.species.url);
-		const speciesPromises = await Promise.allSettled(speciesURL);
-		const jsonSpeciesPromises = speciesPromises
-			.filter((x) => x.status === "fulfilled")
-			.map((x) => x.value.json());
-		const pokeSpeciesInfo = await Promise.all(jsonSpeciesPromises);
+    const speciesURL = pokeInfo.map((x) => fetch(x.species.url));
+    const speciesPromises = await Promise.allSettled(speciesURL);
+    const jsonSpeciesPromises = speciesPromises
+      .filter((x) => x.status === "fulfilled")
+      .map((x) => x.value.json());
+    const pokeSpeciesInfo = await Promise.all(jsonSpeciesPromises);
 
-		console.log(pokeInfo)
-		console.log(pokeSpeciesInfo)
-		const list = [];
-		for (const element of pokeInfo) {
-			const pokemon = { element };
-		}
-	}
+    const merged = pokeInfo.map((p) => {
+      const s = pokeSpeciesInfo.find((s) => s.id === p.id);
+      return {
+        sprite: p.sprites.front_default,
+        id: p.order,
+        name: p.name,
+        types: p.types.map((type) => type.type.name),
+        hasEvo: !!s?.evolves_from_species,
+        evolvesFrom: s?.evolves_from_species?.name,
+      };
+    });
 
-	useEffect(() => {
-		if (!pokenames.length) return;
-		getPokeData();
-	}, [pokenames]);
+    setPokeList(merged);
+  }
 
-	return (
-		<>
-			{/* pasar esto a componente */}
-			<div className="triangle triangle--r"></div>
-			<div className="triangle triangle--l"></div>
-			<div className="circle circle--r"></div>
-			<div className="circle circle--l"></div>
-			<div className="container"></div>
-			{/*                     */}
-			<NavBar>
-				<input
-					className="searchbar"
-					type="text"
-					value={searchStr}
-					onChange={filterStrChanged}
-				/>
-			</NavBar>
-			<main className="pokecard pokecard__grid"></main>
-		</>
-	);
+  useEffect(() => {
+    if (!pokenames.length) return;
+    getPokeData();
+  }, [pokenames]);
+  useEffect(() => {
+    console.log("pokeList actualizada:", pokeList);
+  }, [pokeList]);
+  return (
+    <>
+      {/* pasar esto a componente */}
+      <div className="triangle triangle--r"></div>
+      <div className="triangle triangle--l"></div>
+      <div className="circle circle--r"></div>
+      <div className="circle circle--l"></div>
+      <div className="container"></div>
+      {/*                     */}
+      <NavBar>
+        <input
+          className="searchbar"
+          type="text"
+          value={searchStr}
+          onChange={filterStrChanged}
+        />
+      </NavBar>
+      <main className="pokecard pokecard__grid">
+        {pokeList.map((p) => (
+          <Cards
+            spriteURL={p.sprite}
+            id={p.id}
+            name={p.name}
+            types={p.types}
+            hasEvo={p.hasEvo}
+            evolvesFrom={p.evolvesFrom}
+          />
+        ))}
+      </main>
+    </>
+  );
 }
 
 export default App;
--- practicasReact/Sheet04/src/exercises/Cards.tsx
+++ practicasReact/Sheet04/src/exercises/Cards.tsx
@@ -1,43 +1,46 @@
 function Cards({
-	spriteURL,
-	id,
-	name,
-	types,
-	hasEvo,
-	evolvesFrom,
+  spriteURL,
+  id,
+  name,
+  types,
+  hasEvo,
+  evolvesFrom,
 }: {
-	spriteURL: string;
-	id: string;
-	name: string;
-	types: [];
-	hasEvo: boolean;
-	evolvesFrom: string;
+  spriteURL?: string;
+  id: string;
+  name: string;
+  types: [];
+  hasEvo: boolean;
+  evolvesFrom?: string;
 }) {
-	return (
-		<>
-			<article className="pokecard pokecard__item">
-				<header>
-					<img
-						className="pokecard pokecard__sprite"
-						src={spriteURL}
-						alt={name}
-					/>
-					<p className="pokecard pokecard__id">id/{id}</p>
-				</header>
-				<footer>
-					<h2 className="pokecard pokecard__name">{name}</h2>
-					<p className="pokecard pokecard__name">
-						{types.map((type) => (
-							<span>{type}</span>
-						))}
-					</p>
-					<p className={hasEvo ? "pokecard pokecard__evolution" : "blankspace"}>
-						{hasEvo && "Evolves from"}
-						<strong>{hasEvo && evolvesFrom}</strong>
-					</p>
-				</footer>
-			</article>
-		</>
-	);
+  return (
+    <>
+      <article className="pokecard pokecard__item">
+        <header>
+          <img
+            className="pokecard pokecard__sprite"
+            src={
+              spriteURL ||
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/500px-Pok%C3%A9_Ball_icon.svg.png"
+            } // si no carga, muestra un placeholder
+            alt={name}
+          />
+          <p className="pokecard pokecard__id">id/{id}</p>
+        </header>
+        <footer>
+          <h2 className="pokecard pokecard__name">{name}</h2>
+          <p className="pokecard pokecard__name">
+            {types.map((type) => (
+              <span key={type}>{type}</span>
+            ))}
+          </p>
+          <p className={hasEvo ? "pokecard pokecard__evolution" : "blankspace"}>
+            {hasEvo && "Evolves from"}
+            <strong>{hasEvo && evolvesFrom}</strong>
+          </p>
+        </footer>
+      </article>
+    </>
+  );
 }
 export default Cards;
