0ba90365cb943d1aedb166ee5c28b11de2b4a9fa
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-11-12 19:13:43 +0100

    ejercicio completado

---
+++ practicasJava/Sheet03/ex03/Alumno.java
@@ -0,0 +1,15 @@
+package practicasJava.Sheet03.ex03;
+
+public class Alumno {
+    private String nombre;
+    private int edad;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
