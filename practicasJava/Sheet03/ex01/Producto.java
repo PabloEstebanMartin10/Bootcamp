93e9d567c03e0a350df5a41ffc75a7546430049c
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-11-12 18:45:35 +0100

    ejercicio completado

---
+++ practicasJava/Sheet03/ex01/Producto.java
@@ -0,0 +1,42 @@
+package practicasJava.Sheet03.ex01;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Producto [nombre=" + nombre + ", precio=" + precio + ", stock=" + stock + "]");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+}
